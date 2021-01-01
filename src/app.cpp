cea5a5fdead45885538a7327d897406e1bce3975
emredesu <ahmetemrebenek@gmail.com>
2021-01-01 15:24:22 +0300

    commentated the fps reporter

--- src/app.cpp
+++ src/app.cpp
@@ -400,9 +400,10 @@ void App::main_loop() {
 	auto frame_begin_time = std::chrono::system_clock::now(); // Frame start time is now.
 	auto frame_end_time = frame_begin_time + casted_fps_limit; // Next frame will be in (time it took to process this frame) + 1000/FPS_LIMIT milliseconds.
 
-
+	/* Uncomment this and the part inside the loop below to get reported of the framerate every second.
 	unsigned int frame_count_per_second = 0;
 	auto previous_time_in_seconds = std::chrono::time_point_cast<std::chrono::seconds>(frame_begin_time);
+	*/
 
 	while (app_active) {
 		if (std::chrono::system_clock::now() < frame_end_time) // If it's not the time for the next frame, wait until we're there.
@@ -415,6 +416,7 @@ void App::main_loop() {
 		update();
 		render();
 		
+		/*
 		auto time_in_seconds = std::chrono::time_point_cast<std::chrono::seconds>(std::chrono::system_clock::now());
 		++frame_count_per_second;
 		if (time_in_seconds > previous_time_in_seconds) {
@@ -422,6 +424,7 @@ void App::main_loop() {
 			frame_count_per_second = 0;
 			previous_time_in_seconds = time_in_seconds;
 		}
+		*/
 	}
 }
 
